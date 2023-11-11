16e662bd1de683d6379d954c6b22a9fcc477d5cf
Terrance Finley <112042894+TerranceFinleyZ@users.noreply.github.com>
2023-11-11 11:17:47 -0700

    Add files via upload

---
+++ page.js
@@ -0,0 +1,77 @@
+import React from 'react'
+
+import { Footer, FooterBanner, HeroBanner } from '../components'
+
+const Home = () => {
+  return (
+    <>
+     <HeroBanner />
+     <div className="products-heading">
+      <p><span>18+</span> only</p>
+      <h2>Bongs</h2>
+     </div> 
+
+     <div className="products-container">
+      <div className="products-card">
+      <img src={"https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcQm1ZrBXEzmhPNLI-n2ub34QzSud2x110UmX3yF3_leL9vPHGy9E-IH06DwcP4OlrGO-A27ZB8VmQgWruqqgk_wtEAiU5XNIvn5J_dvmmazkFT2aAjfErYcfg&amp;usqp=CAE"}  data-was-processed="true" width={250} height={250} className="product-image" />
+          <p className="product-image">	
+HALO TIE DYE BEAKER DOUBLE STACK</p>
+          <p className="product-price">$344.90 <span style={{textDecoration: 'line-through'}}>
+          $367.46
+        </span></p>
+      </div>
+      <div className="products-card">
+      <img src={"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQERoPECFpplaW9NGPbm3ME2hnZbXEfQeea0gEsdGoc4KCpXgss0Z_Jj_hIGedv8MKs-wS9YlxXIxHGb2qKYdmvFvWF4_mszimX2VWNjCKy6l8Q3iLl0u0Msg&amp;usqp=CAE"}  data-was-processed="true" width={250} height={250} className="product-image" />
+          <p className="product-image">	
+HALO TIE DYE BEAKER DOUBLE STACK</p>
+          <p className="product-price">$344.90 <span style={{textDecoration: 'line-through'}}>
+          $367.46
+        </span></p>
+      </div>
+      <div className="products-card">
+      <img src={"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQERoPECFpplaW9NGPbm3ME2hnZbXEfQeea0gEsdGoc4KCpXgss0Z_Jj_hIGedv8MKs-wS9YlxXIxHGb2qKYdmvFvWF4_mszimX2VWNjCKy6l8Q3iLl0u0Msg&amp;usqp=CAE"}  data-was-processed="true" width={250} height={250} className="product-image" />
+          <p className="product-image">	
+HALO TIE DYE BEAKER DOUBLE STACK</p>
+          <p className="product-price">$344.90 <span style={{textDecoration: 'line-through'}}>
+          $367.46
+        </span></p>
+      </div>
+      <div className="products-card">
+      <img src={"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQERoPECFpplaW9NGPbm3ME2hnZbXEfQeea0gEsdGoc4KCpXgss0Z_Jj_hIGedv8MKs-wS9YlxXIxHGb2qKYdmvFvWF4_mszimX2VWNjCKy6l8Q3iLl0u0Msg&amp;usqp=CAE"}  data-was-processed="true" width={250} height={250} className="product-image" />
+          <p className="product-image">	
+HALO TIE DYE BEAKER DOUBLE STACK</p>
+          <p className="product-price">$344.90 <span style={{textDecoration: 'line-through'}}>
+          $367.46
+        </span></p>
+      </div>
+      <div className="products-card">
+      <img src={"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQERoPECFpplaW9NGPbm3ME2hnZbXEfQeea0gEsdGoc4KCpXgss0Z_Jj_hIGedv8MKs-wS9YlxXIxHGb2qKYdmvFvWF4_mszimX2VWNjCKy6l8Q3iLl0u0Msg&amp;usqp=CAE"}  data-was-processed="true" width={250} height={250} className="product-image" />
+          <p className="product-image">	
+HALO TIE DYE BEAKER DOUBLE STACK</p>
+          <p className="product-price">$344.90 <span style={{textDecoration: 'line-through'}}>
+          $367.46
+        </span></p>
+      </div>
+      <div className="products-card">
+      <img src={"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQERoPECFpplaW9NGPbm3ME2hnZbXEfQeea0gEsdGoc4KCpXgss0Z_Jj_hIGedv8MKs-wS9YlxXIxHGb2qKYdmvFvWF4_mszimX2VWNjCKy6l8Q3iLl0u0Msg&amp;usqp=CAE"}  data-was-processed="true" width={250} height={250} className="product-image" />
+          <p className="product-image">	
+HALO TIE DYE BEAKER DOUBLE STACK</p>
+          <p className="product-price">$344.90 <span style={{textDecoration: 'line-through'}}>
+          $367.46
+        </span></p>
+      </div>
+      <div className="products-card">
+      <img src={"https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQERoPECFpplaW9NGPbm3ME2hnZbXEfQeea0gEsdGoc4KCpXgss0Z_Jj_hIGedv8MKs-wS9YlxXIxHGb2qKYdmvFvWF4_mszimX2VWNjCKy6l8Q3iLl0u0Msg&amp;usqp=CAE"}  data-was-processed="true" width={250} height={250} className="product-image" />
+          <p className="product-image">	
+HALO TIE DYE BEAKER DOUBLE STACK</p>
+          <p className="product-price">$344.90 <span style={{textDecoration: 'line-through'}}>
+          $367.46
+        </span></p>
+      </div>
+     </div>
+     <Footer />
+    </>
+  )
+}
+
+export default Home
