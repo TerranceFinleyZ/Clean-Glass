76069f6b372480a5e60afd3a597dc2faab84f7ee
Terrance Finley <112042894+TerranceFinleyZ@users.noreply.github.com>
2023-11-15 17:05:46 -0700

    Add files via upload

---
+++ HeroBanner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroBanner = () => {
+  return (
+    <div className="hero-banner-container">
+      <div>
+          <h3>Clean Glass</h3>
+      </div>
+    </div>
+  )
+}
+
+export default HeroBanner
